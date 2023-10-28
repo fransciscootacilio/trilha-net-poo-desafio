80098cf13935a672d3371dfd87f7258ecf9dfa19
franciscootacilio <franciscootacilio@gmail.com>
2023-10-28 19:54:19 -0300

    Projeto POO celular Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,49 @@
-﻿using DesafioPOO.Models;
+﻿using System.Runtime.Serialization;
+using ExemploPOO.Interfaces;
+using ExemploPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+ICalculadora calc = new Calculadora();
+Console.WriteLine(calc.Somar(3, 2));
+
+
+
+//Computador c = new Computador();
+
+
+
+// Pessoa p1 = new Pessoa("Leonardo");
+// Aluno a1 = new Aluno("Francisco");
+// a1.Apresentar();
+
+// Corrente c = new Corrente();
+// c.Creditar(100);
+// c.ExibirSaldo();
+
+
+// Aluno a1 = new Aluno();
+// Professor p2 = new Professor();
+
+// a1.Nome = "Francisco";
+// a1.Idade = 20;
+// a1.Nota = 7;
+
+// p2.Nome = "Valéria";
+// p2.Idade = 42;
+// p2.Salario = 1500;
+
+// a1.Apresentar();
+// p2.Apresentar();
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 100);
+
+// c1.ExibirSaldo();
+// c1.Sacar(500);
+// c1.ExibirSaldo();
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Leonardo";
+// p1.Idade = 20;
+
+// p1.Apresentar();
